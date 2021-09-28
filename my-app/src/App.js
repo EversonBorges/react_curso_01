bb3cd535bc7325435ebcd0cc5bf0707a5a77e8de
EversonBorges <everson.cursos@gmail.com>
2021-09-28 20:41:42 -0300

    utilizando states e propos

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
+import ListaDeNotas from "./components/ListaDeNotas";
 import FormularioCadastro from "./components/FormularioCadastro";
-import ListaNotas from "./components/ListaNotas"
+import "./assets/App.css";
+import './assets/index.css';
+class App extends Component {
 
-function App() {
-  return (
-    <section>
-      <FormularioCadastro/>
-     <ListaNotas/>
-    </section>
-  );
+  constructor(){
+    super();
+  
+    this.state = {
+      notas:[]
+    };
+  }
+
+  criarNota(titulo, texto){
+    const nota = {titulo, texto}
+    const newArrayNotas = [...this.state.notas, nota]
+    const newState = {
+      notas:newArrayNotas
+    }
+
+    this.setState(newState)
+  }
+  render() {
+    return (
+      <section className="conteudo">
+        <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
+        <ListaDeNotas notas={this.state.notas}/>
+      </section>
+    );
+  }
 }
 
 export default App;
